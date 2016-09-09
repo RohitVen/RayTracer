c926a20fe19bbf7c0becc0c8129d71be17512bbe
Rohit Venugopal <rohitven@cs.utexas.edu>
2016-09-09 11:35:56 -0500

    First commit

---
+++ ray/build/src/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/RayTracer.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/RayTracer.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/Box.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/Cone.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/Cylinder.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/Sphere.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/Square.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/SceneObjects/trimesh.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/fileio/bitmap.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/fileio/bitmap.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/fileio/buffer.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/fileio/buffer.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/fileio/pngimage.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/fileio/pngimage.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/main.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/main.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/parser/Parser.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/parser/Parser.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/parser/ParserException.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/parser/ParserException.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/parser/Token.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/parser/Token.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/parser/Tokenizer.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/camera.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/camera.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/cubeMap.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/cubeMap.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/light.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/light.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/material.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/material.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/ray.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/ray.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/scene/scene.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/scene/scene.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/CommandLineUI.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/CubeMapChooser.cxx" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/GraphicalUI.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/ModelerCamera.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/TraceGLWindow.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/debuggingView.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingView.cpp.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/debuggingWindow.cxx" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o"
+  "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/src/ui/glObjects.cpp" "/v/filer4b/v38q001/rohitven/Desktop/CS354/A1/ray/build/src/CMakeFiles/ray.dir/ui/glObjects.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/lib/fltk"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
